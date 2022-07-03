0a0401b702b580ba82c6a19bf9f8876f6b58c282
Surya Diputra <surya.diputra@spinroam.com>
2022-07-03 19:11:33 +0800

    add width height

--- components/Model.tsx
+++ components/Model.tsx
@@ -1,12 +1,13 @@
 import "@google/model-viewer";
 
 const Model = () => (
-  <div id="card">
+  <div>
     {/* @ts-ignore  */}
     <model-viewer
       src="/models/ar.usdz"
       ios-src="/models/ar.usdz"
       alt="A 3D model of an astronaut"
+      poster="https://cdn.glitch.com/36cb8393-65c6-408d-a538-055ada20431b%2Fposter-astronaut.png?v=1599079951717"
       shadow-intensity="1"
       camera-controls
       auto-rotate
